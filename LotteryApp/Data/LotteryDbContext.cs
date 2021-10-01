26869f74d3d2f18f87660602c8b97aedaaf9ce1c
mslowik <mslowik@loken.pl>
2021-10-01 06:17:12 +0200

    database configuration

--- LotteryApp/Data/LotteryDbContext.cs
+++ LotteryApp/Data/LotteryDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using LotteryApp.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,19 @@ namespace LotteryApp.Data
 {
     public class LotteryDbContext : DbContext
     {
+        private readonly IConfiguration _config;
+
+        public LotteryDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
         public DbSet<DrawHistory> DrawHistories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseSqlServer();
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:LotteryAppDbContext"]);
         }
     }
 }
---
+++ LotteryApp/Migrations/20211001041430_InitialDb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LotteryApp.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DrawHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Draw = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrawHistories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DrawHistories");
+        }
+    }
+}
